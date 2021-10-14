1d93cbf6ccbc18f45f74115300fa9259dcb40f23
atiqah <hello@flowerpetalsofdoom.com>
2021-10-14 22:29:21 +0800

    add google maps

---
+++ src/components/Error.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Navbar from "./Navbar";
+
+function Error() {
+  return (
+    <div className="about">
+      <Navbar />
+      <h1>ERROR</h1>
+    </div>
+  );
+};
+
+export default Error;
