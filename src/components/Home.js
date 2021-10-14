d4a4eb82ba5c668485fa6c0e137654c6c02724ce
atiqah <atiqahhafiz@convertium.com>
2021-10-14 17:32:18 +0800

    Add react-router
    - Add navigation
    - Add other pages

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Header from "./Header";
+
+function Home() {
+  return (
+    <div class="home">
+      <Header />
+      <h1>Home</h1>
+    </div>
+  );
+};
+
+export default Home;
