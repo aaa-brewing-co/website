6c7a84747181590160c68c0f225d4f8969fe46f8
Agnes <agneschong@convertium.com>
2021-10-30 23:21:36 +0800

    add accordion

--- src/components/Beer/index.js
+++ src/components/Beer/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 
 import Navbar from "../commons/Navbar";
+import BeerType from "../commons/beer-type";
 import cat from '../../assets/cat.jpg';
 
 function About() {
   return (
     <div className="about">
       <Navbar />
+      <h1>Beer Styles</h1>
+      <BeerType />
       <div className="main">
         <h1>What is Craft Beer?</h1>
         <p>blah blah blah</p>
