57a2273e6f333bcfea74e12d30b040c3f49ee05b
aungkyawhein <akh.infotech@gmail.com>
2021-11-04 01:18:38 +0800

    update google map

--- src/components/Locations/GoogleMap.js
+++ src/components/Locations/GoogleMap.js
@@ -1,66 +1,62 @@
-import React, { useRef, useEffect, useState } from "react";
+import React, { useRef, useEffect } from "react";
 import { Wrapper, Status } from "@googlemaps/react-wrapper";
 
 export default function MapWrapper() {
   const center = { lat: 1.3654, lng: 103.8436 };
   const zoom = 12;
-  const kanpai = { lat: 1.2838, lng: 103.8485 };
+
+  const render = function (status) {
+    if (status === Status.LOADING) return <h3>{status} ..</h3>;
+    if (status === Status.FAILURE) return <h3>{status} ...</h3>;
+    return null;
+  };
 
   return (
     <Wrapper apiKey={process.env.REACT_APP_GMAPS_API_KEY} render={render}>
-      <Map center={center} zoom={zoom} >
-        <Marker position={kanpai} />
-      </Map>
+      <Map center={center} zoom={zoom} />
     </Wrapper>
   );
 }
 
-const render = function (status) {
-  if (status === Status.LOADING) return <h3>{status} ..</h3>;
-  if (status === Status.FAILURE) return <h3>{status} ...</h3>;
-  return null;
-};
-
 function Map({ center, zoom, children }) {
   const ref = useRef();
-  const [map, setMap] = useState();
+  const locations = [
+    { lat: 1.2838, lng: 103.8485 },
+    { lat: 1.3654, lng: 103.8436 }
+  ];
 
   useEffect(() => {
-    if (ref.current && !map) {
-      setMap(new window.google.maps.Map(ref.current, {
-        center,
-        zoom
-      }));
-    }
-  }, [ref, map, center, zoom]);
+    const map = new window.google.maps.Map(ref.current, {
+      center,
+      zoom
+    });
+    locations.forEach(location => {
+      new window.google.maps.Marker({
+        position: location,
+        map: map,
+      });
+    });
+  }, [ref, locations, center, zoom]);
 
   return (
-    <>
-      <div ref={ref} id="map" />
-      {React.Children.map(children, child => {
-        if (React.isValidElement(child)) {
-          // set the map prop on the child component
-          return React.cloneElement(child, { map }, null);
-        }
-      })}
-    </>
+    <div ref={ref} id="map" />
   );
 }
 
-function Marker ({ position }) {
-  const [marker, setMarker] = useState();
+// function Marker ({ position }) {
+//   const [marker, setMarker] = useState();
 
-  useEffect(() => {
-    if (!marker) {
-      setMarker(new window.google.maps.Marker());
-    }
-  }, [marker]);
+//   useEffect(() => {
+//     if (!marker) {
+//       setMarker(new window.google.maps.Marker());
+//     }
+//   }, [marker]);
 
-  useEffect(() => {
-    if (marker) {
-      marker.setOptions(position);
-    }
-  }, [marker, position]);
+//   useEffect(() => {
+//     if (marker) {
+//       marker.setOptions(position);
+//     }
+//   }, [marker, position]);
 
-  return null;
-}
+//   return null;
+// }
