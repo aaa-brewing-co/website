39c01987d041c86be2a76ea8b2b0675b00f03a68
aungkyawhein <akh.infotech@gmail.com>
2021-11-11 00:59:41 +0800

    beer styles filters

--- src/components/Beer/index.js
+++ src/components/Beer/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import './style.scss';
 
@@ -6,18 +6,33 @@ import Navbar from "../commons/Navbar";
 import DropdownFilter from "../commons/DropdownFilter";
 // import BeerTypes from "./BeerTypes";
 import BeerStyles from "./BeerStyles";
+import { beers } from "./beers";
 
-const filterOptions = [ "India Pale Ale", "Stout", "Pale Ale", "Sours", "Porter", "Pilsner", "Belgian", "Wheat" ]
+const filterOptions = beers.map(beer => beer.title);
 
 export default function Beer() {
+  const [filteredBeers, updatefilteredBeers] = useState(beers);
+
+  function filterSelect(filteredBeer) {
+    if (filteredBeer) {
+      updatefilteredBeers(beers.filter(beer => beer.title === filteredBeer));
+    } else {
+      updatefilteredBeers(beers)
+    }
+  }
+
   return (
     <div className="beer">
       <Navbar />
       <div className="section main">
         <h2 className="heading is-2">Most Common Beer Styles</h2>
-        <DropdownFilter data={filterOptions} useDefault={false} />
+        <DropdownFilter
+          data={filterOptions}
+          useDefault={true}
+          onFilterSelect={filterSelect}
+        />
 
-        <BeerStyles />
+        <BeerStyles beers={filteredBeers}/>
         {/* <BeerTypes /> */}
 
         <h2 className="heading is-2">Tasting Guide</h2>
